b2aba0c0d740e1685126d4c247cdaa6e557eb540
Virginia Covert <virginiacovert454@gmail.com>
2025-09-29 08:57:37 -0400

    Updated to working file linking with optional preview

--- main.ts
+++ main.ts
@@ -1,134 +1,130 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Plugin, Setting } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface LinkContentDictionary {
+  [key: string]: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+function generateTable(fileName: string, windowsLink: string, macLink: string, mobileLink: string, preview: boolean): string {
+	if (preview) {
+		return `|                                                                                           | File: ${fileName}                                                                                           |            |
+| ----------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------- | ---------- |
+| ![Windows](${windowsLink}) | ![MacOS](${macLink})<br> | ![Mobile](${mobileLink}) |`
+	} else {
+		return `|                                                                                           | File: ${fileName}                                                                                           |            |
+| ----------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------------------------- | ---------- |
+| [Windows](${windowsLink}) | [MacOS](${macLink})<br> | [Mobile](${mobileLink}) |`
+	}
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+function generateLinkContent(path: string): LinkContentDictionary {
+	// Remove all double-quotes from the path
+	path = path.replace(/"/g, '');
+	
+	let windowsLink = '';
+	let macLink = '';
+	let mobileLink = 'Not supported yet';
+	
+	if (path.contains('Z:') || path.contains('z:')) {
+		// Windows path was provided
+		const cleanPath = path.replace(/\\/g, '/');
+		windowsLink = `<file:///${cleanPath}>`;
+		
+		// Convert to Mac path: Z:/Shared/... -> /Volumes/Files/Shared/...
+		const relativePath = cleanPath.substring(2); // Remove "Z:"
+		macLink = `<file:///Volumes/Files${relativePath}>`;
+		
+	} else if (path.contains('/Volumes/Files')) {
+		// Mac path was provided
+		macLink = `<file://${path}>`;
+		
+		// Convert to Windows path: /Volumes/Files/Shared/... -> Z:/Shared/...
+		const relativePath = path.substring('/Volumes/Files'.length); // Remove "/Volumes/Files"
+		const windowsPath = `Z:${relativePath}`;
+		windowsLink = `<file:///${windowsPath}>`;
+	}
 
+	const links: LinkContentDictionary = {
+		"windows": windowsLink,
+		"mac": macLink,
+		"mobile": mobileLink
+	};
+
+	return links;
+}
+
+function createContent(path: string, preview: boolean): string {
+	const fileName = (path.split('\\').pop()?.split('/').pop() || 'Unknown File').replace(/"/g, '');
+	const links = generateLinkContent(path);
+	
+	return generateTable(fileName, links.windows, links.mac, links.mobile, preview);
+}
+
+export default class GinnyPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (_evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
+		/* The command below will insert the links to each of the NAS files by OS
+		 * The supported OS options:
+		 * - Windows
+		 * - Mac
+		 * - Mobile (placeholder)
+		 */
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
+			id: 'add-nas-links',
+			name: 'Add NAS Links',
 			editorCallback: (editor: Editor, _view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
+				new NASLinksModal(this.app, (result, preview) => {
+					const content = createContent(result, preview);
+					editor.replaceRange(content, editor.getCursor());
+				}).open();
+
+				return true;
 			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		})
 	}
 
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
+	onunload() {}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
+// Ex. Dark Matter PDF Links
+// Input: 
+// Z:\Shared\DnD\Current Campaign\Dark Matter Campaign\Dark Matter.pdf
+//
+// Output:
+// [Windows](<file:///Z:/Shared/DnD/Current Campaign/Dark Matter Campaign/Dark Matter.pdf>)
+// [MacOS](<file:///Volumes/Files/Shared/DnD/Current Campaign/Dark Matter Campaign/Dark Matter.pdf>)
+
+export class NASLinksModal extends Modal {
+	onSubmit: (result: string, preview: boolean) => void;
+
+	constructor(app: App, onSubmit: (result: string, preview: boolean) => void) {
 		super(app);
-	}
+		this.onSubmit = onSubmit;
+		this.setTitle('Input File Path');
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
+		let path = '';
+		new Setting(this.contentEl)
+			.setName('Path')
+			.addText((text) =>
+				text.onChange((value) => {
+					path = value;
 				}));
+
+		let preview = false;
+		new Setting(this.contentEl)
+                .setName('Enable Preview')
+                .addToggle(toggle => toggle
+                    .setValue(false) // Initial state of the checkbox
+                    .onChange((value) => {
+                        preview = value;
+                    }));
+
+		new Setting(this.contentEl)
+			.addButton((btn) =>
+				btn
+					.setButtonText('Submit')
+					.setCta()
+					.onClick(() => {
+						this.close();
+						this.onSubmit(path, preview);
+					}));
 	}
 }
